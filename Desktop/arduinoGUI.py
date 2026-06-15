35c8195c8e81a68c27c9cddc418193fef21c5375
KreathDesu <k3v1ns1mb0b@gmail.com>
2026-06-15 17:46:37 +0200

    updated GUI

--- Desktop/arduinoGUI.py
+++ Desktop/arduinoGUI.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 import json
 import serial
+import time
 
 #------------Arduino connection--------------
 
-ser = serial.Serial("COM5", 115200)
+ser = serial.Serial("COM3", 115200)
 
 dataToSend = {
     "isFiring": False,
@@ -14,46 +15,87 @@ dataToSend = {
     "intensityPercent": 0
 }
 
+# Wird eigentlich nur für Scopen verwendet, unwichtig auf Arduino
+prevDataToSend = {
+    "isFiring": False,
+    "firingPeriod": 10,
+    "firingPattern": 0,
+    "ammunitionPercent": 0,
+    "intensityPercent": 0
+}
+
 #-----------------Functions-----------------
 
-def updateOutput():
-    print(dataToSend)
-    # line = ser.readline()
-    # if line:
-    #     text = line.decode('utf-8', errors='ignore').strip()
-    #     if text:
-    #         print(text)
+def saveData():
+    prevDataToSend["isFiring"] = dataToSend["isFiring"]
+    prevDataToSend["firingPeriod"] = dataToSend["firingPeriod"]
+    prevDataToSend["firingPattern"] = dataToSend["firingPattern"]
+    prevDataToSend["ammunitionPercent"] = dataToSend["ammunitionPercent"]
+    prevDataToSend["intensityPercent"] = dataToSend["intensityPercent"]
+
+def loadData():
+    dataToSend["isFiring"] = prevDataToSend["isFiring"]
+    dataToSend["firingPeriod"] = prevDataToSend["firingPeriod"]
+    dataToSend["firingPattern"] = prevDataToSend["firingPattern"]
+    dataToSend["ammunitionPercent"] = prevDataToSend["ammunitionPercent"]
+    dataToSend["intensityPercent"] = prevDataToSend["intensityPercent"]
 
 def vibrationON():
     dataToSend["isFiring"] = True
     json_string:str = json.dumps(dataToSend) + "\n"
     ser.write(json_string.encode())
-    updateOutput()
+    print(dataToSend)
+
 def vibrationOFF():
     dataToSend["isFiring"] = False
     json_string:str = json.dumps(dataToSend) + "\n"
     ser.write(json_string.encode())
-    updateOutput()
-def setAmmoPercent():
-    dataToSend["ammunitionPercent"] = int(AMMOPercent.get())
+    print(dataToSend)
+
+def scopeONCE():
+    saveData()
+    dataToSend["isFiring"] = True
+    dataToSend["ammunitionPercent"] = 100
+    dataToSend["firingPeriod"] = 25
+    dataToSend["intensityPercent"] = 100
+    dataToSend["firingPattern"] = 0
     json_string:str = json.dumps(dataToSend) + "\n"
     ser.write(json_string.encode())
-    updateOutput()
-def updatePattern():
-    dataToSend["firingPattern"] = int(FIRINGPattern.get())
+    time.sleep(0.025)
+    dataToSend["isFiring"] = False
     json_string:str = json.dumps(dataToSend) + "\n"
     ser.write(json_string.encode())
-    updateOutput()
-def updateIntensity():
-    dataToSend["intensityPercent"] = int(INTENSITYPercent.get())
+    print(dataToSend)
+    loadData()
+
+def scopeTWICE():
+    scopeONCE()
+    time.sleep(0.25)
+    scopeONCE()
+
+def updateAmmoPercent(val):
+    dataToSend["ammunitionPercent"] = int(val)
     json_string:str = json.dumps(dataToSend) + "\n"
     ser.write(json_string.encode())
-    updateOutput()
-def updatePeriod():
-    dataToSend["firingPeriod"] = int(FIRINGPeriod.get())
+    print(dataToSend)
+
+def updatePattern(val):
+    dataToSend["firingPattern"] = int(val)
     json_string:str = json.dumps(dataToSend) + "\n"
     ser.write(json_string.encode())
-    updateOutput()
+    print(dataToSend)
+
+def updateIntensity(val):
+    dataToSend["intensityPercent"] = int(val)
+    json_string:str = json.dumps(dataToSend) + "\n"
+    ser.write(json_string.encode())
+    print(dataToSend)
+
+def updatePeriod(val):
+    dataToSend["firingPeriod"] = int(val)
+    json_string:str = json.dumps(dataToSend) + "\n"
+    ser.write(json_string.encode())
+    print(dataToSend)
 
 #Sine, SawUp, SawDown, HeavyShot
 
@@ -62,42 +104,32 @@ win = tk.Tk()
 win.title("CS haptic feedback debug")
 win.minsize(240,240)
 
-buttonLabel = tk.Label(win, text="Click to turn ON/OFF")
-buttonLabel.grid(column=1, row=1)
+ONbtn = tk.Button(win, bd=4, text="Firing ON", command=vibrationON)
+ONbtn.grid(column=1, row=1)
+OFFbtn = tk.Button(win, bd=4, text="Firing OFF", command=vibrationOFF)
+OFFbtn.grid(column=2, row=1)
 
-ONbtn = tk.Button(win, bd=4, text="TEST AR ON", command=vibrationON)
-ONbtn.grid(column=1, row=2)
-OFFbtn = tk.Button(win, bd=4, text="TEST AR OFF", command=vibrationOFF)
-OFFbtn.grid(column=2, row=2)
+tk.Label(win, text="").grid(column=1, row=2)
 
-AMMOPercent = tk.Scale(win, bd=5, from_=0, to=100, orient=tk.HORIZONTAL)
-AMMOPercent.grid(column=1, row=3)
-tk.Label(win, text="Ammo (%)").grid(column=2, row=3)
-AMMOPercentUpdate = tk.Button(win, bd=4, text="Update", command=setAmmoPercent)
-AMMOPercentUpdate.grid(column=1, row=4)
+SCOPEONCEbtn = tk.Button(win, bd=4, text="Scope once", command=scopeONCE)
+SCOPEONCEbtn.grid(column=1, row=3)
+SCOPETWICEbtn = tk.Button(win, bd=4, text="Scope twice", command=scopeTWICE)
+SCOPETWICEbtn.grid(column=2, row=3)
 
-FIRINGPattern = tk.Scale(win, bd=5, from_=0, to=4, orient=tk.HORIZONTAL)
+AMMOPercent = tk.Scale(win, bd=5, from_=0, to=100, orient=tk.HORIZONTAL, command=updateAmmoPercent)
+AMMOPercent.grid(column=1, row=4)
+tk.Label(win, text="Ammo (%)").grid(column=2, row=4)
+
+FIRINGPattern = tk.Scale(win, bd=5, from_=0, to=4, orient=tk.HORIZONTAL, command=updatePattern)
 FIRINGPattern.grid(column=1, row=5)
 tk.Label(win, text="0 = Constant; 1 = SinePulse;\n2 = SawUp; 3 = SawDown; 4 = HeavyShot").grid(column=2, row=5)
-FIRINGPatternUpdate = tk.Button(win, bd=4, text="Update", command=updatePattern)
-FIRINGPatternUpdate.grid(column=1, row=6)
 
-INTENSITYPercent = tk.Scale(win, bd=5, from_=0, to=100, orient=tk.HORIZONTAL)
-INTENSITYPercent.grid(column=1, row=7)
-tk.Label(win, text="Intensity (%)").grid(column=2, row=7)
-INTENSITYPercentUpdate = tk.Button(win, bd=4, text="Update", command=updateIntensity)
-INTENSITYPercentUpdate.grid(column=1, row=8)
+INTENSITYPercent = tk.Scale(win, bd=5, from_=0, to=100, orient=tk.HORIZONTAL, command=updateIntensity)
+INTENSITYPercent.grid(column=1, row=6)
+tk.Label(win, text="Intensity (%)").grid(column=2, row=6)
 
-FIRINGPeriod = tk.Scale(win, bd=5, from_=10, to=2000, orient=tk.HORIZONTAL)
-FIRINGPeriod.grid(column=1, row=9)
-tk.Label(win, text="Firing period (ms)").grid(column=2, row=9)
-FIRINGPeriodUpdate = tk.Button(win, bd=4, text="Update", command=updatePeriod)
-FIRINGPeriodUpdate.grid(column=1, row=10)
-
-# FIRINGPattern = tk.Entry(win, bd=6, width=12)
-# FIRINGPattern.grid(column=1, row=5)
-# tk.Label(win, text="Choose firing pattern").grid(column=2, row=5)
-# FIRINGPatternUpdate = tk.Button(win, bd=4, text="Update", command=updatePattern)
-# FIRINGPatternUpdate.grid(column=1, row=6)
+FIRINGPeriod = tk.Scale(win, bd=5, from_=10, to=2000, orient=tk.HORIZONTAL, command=updatePeriod)
+FIRINGPeriod.grid(column=1, row=7)
+tk.Label(win, text="Firing period (ms)").grid(column=2, row=7)
 
 win.mainloop()
